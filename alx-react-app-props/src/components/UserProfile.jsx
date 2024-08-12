e5ca6f56e9a1ff20dce8c7aae1c50c286b24ad63
Nicole Rebecca <nicolerebecca27@gmail.com>
2024-08-12 13:14:17 +0300

    Modify

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,13 @@
+import {useContext} from 'react';
+import UserContext from './UserContext';
 
-import React from 'react';
-
-const UserProfile = (props) => {
+const UserProfile = () => {
+    const userData = useContext(UserContext);
     return (
         <div>
-            <h2>{props.name}</h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
+            <h2>User Profile</h2>
+            <p>Name: {userData.name}</p>
+            <p>Email: {userData.email}</p>
         </div>
     );
 };
