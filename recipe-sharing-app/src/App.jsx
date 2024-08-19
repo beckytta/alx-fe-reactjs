bf6246eedec94119756a7c819430dae13ed09c9e
Nicole Rebecca <nicolerebecca27@gmail.com>
2024-08-19 12:51:48 +0300

    Modify

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
