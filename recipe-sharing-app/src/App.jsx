cf708a686b279bcbcc49ff7e468393b31142f820
Nicole Rebecca <nicolerebecca27@gmail.com>
2024-08-19 12:54:55 +0300

    Modify

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
+import AddRecipeForm from './components/AddRecipeForm';
 
 const App = () => {
   return (
     <div>
-      <h1>Recipe Sharing Application</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/add-recipe">Add Recipe</Link>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
+        </Routes>
+      </main>
     </div>
   );
 };
