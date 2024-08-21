594f398e733e8e4bee0226d7966153eb6bdc99ca
Nicole Rebecca <nicolerebecca27@gmail.com>
2024-08-21 13:42:52 +0300

    Modify files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import AddRecipeForm from './components/AddRecipeForm';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
   return (
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,17 +2,23 @@ import create from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (id) => set(state => ({
-    recipes: state.recipes.filter(recipe => recipe.id !== id)
+  searchTerm: '',
+  filteredRecipes: [],
+
+  setSearchTerm: (term) => set(state => {
+    const filtered = state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(term.toLowerCase())
+    );
+    return { searchTerm: term, filteredRecipes: filtered };
+  }),
+
+  setRecipes: (recipes) => set(state => ({
+    recipes: recipes,
+    filteredRecipes: recipes // Initialize filtered recipes with all recipes
   })),
-  updateRecipe: (updatedRecipe) => set(state => ({
-    recipes: state.recipes.map(recipe =>
-      recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-    )
-  })),
-  setRecipes: (recipes) => set({ recipes })
 }));
 
 export default useRecipeStore;
+
+
 
