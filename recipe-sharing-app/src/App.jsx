3d24a93aeba9d0df995022a923fad30f713a6bb1
Nicole Rebecca <nicolerebecca27@gmail.com>
2024-08-21 13:50:27 +0300

    modify files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import RecipeDetails from './components/RecipeDetails';
 import AddRecipeForm from './components/AddRecipeForm';
@@ -15,6 +15,7 @@ const App = () => {
         </nav>
       </header>
       <main>
+        <SearchBar />
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
--- recipe-sharing-app/src/components/SearchBar.jsx
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -1,14 +1,15 @@
-import React
-import useRecipeStore from "./recipeStore";
+import React from 'react';
+import useRecipeStore from './recipeStore';
 
-const SearchBar = ()=> {
+const SearchBar = () => {
   const setSearchTerm = useRecipeStore(state => state.setSearchTerm);
-   
-  return(
+
+  return (
     <input
-    type= "text"
-    placeholder="Search recpies..."
-    onChange={(e) => setSearchTerm(e.target.value)}/>
+      type="text"
+      placeholder="Search recipes..."
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
   );
 };
 
