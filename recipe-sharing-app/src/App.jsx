6a7a64545fc3d465aff096bb43c09044a954482a
Nicole Rebecca <nicolerebecca27@gmail.com>
2024-08-21 14:35:48 +0300

    Modify files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,15 +3,30 @@ import SearchBar from './components/SearchBar';
 import RecipeList from './components/RecipeList';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
-import { Router, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
+      <header>
+        <nav>
+          <Link to = "/">Home</Link>
+          <Link to = "/add-recipe">Add Recipe</Link>
+        </nav>
+      </header>
+      <main>
       <h1>Recipe Sharing App</h1>
       <RecipeList />
       <FavoritesList />
       <RecommendationsList />
+      
+      <SearchBar />
+      <Routes>
+      <Route path="/" element={<RecipeList />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
+        </Routes>
+      </main>
     </div>
   );
 };
