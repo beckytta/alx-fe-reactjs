3d5564abb4b806c39a98d7e6c84fff8cf9409973
Nicole Rebecca <nicolerebecca27@gmail.com>
2024-09-17 14:09:31 +0300

    Modify

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -21,7 +21,7 @@ const Search = () => {
       const data = await fetchUserData(username);
       setUserData(data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError["Looks like we can't find the user"];
       setUserData(null);
     } finally {
       setLoading(false);
